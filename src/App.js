ab2952ba39634f0803fae6d1468740f94b31cfee
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-05-12 22:35:06 -0400

    custom UI for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
 import './scss/style.scss';
@@ -27,19 +28,20 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-          <React.Suspense fallback={loading}>
-            <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
+        <React.Suspense fallback={loading}>
+          <Switch>
+            <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />
+            <Route exact path="/register" name="Register Page" render={props => <Register {...props} />} />
+            <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
+            <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
+            <Route path="/dashboard" name="Home" render={props => <TheLayout {...props} />} />
+            <Route path="/" name="Login" render={props => <Login {...props} />} />
+          </Switch>
+        </React.Suspense>
       </HashRouter>
     );
   }
 }
 
-export default withAuthenticator(App);
+export default (App);
 // export default App;
