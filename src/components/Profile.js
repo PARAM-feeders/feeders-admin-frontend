c902902f891254d59c635087ad7f7088c4c6830f
Rajith <rajith.venu-gopal@mocaclinic.ca>
2021-07-15 23:41:52 -0400

    profile loader

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
 import AuthService from "../utils/AuthService";
-
+import { CSpinner } from "@coreui/react";
 const Profile = () => {
 
-
+  const [loading, setLoading] = useState(true);
   const [userMetadata, setUserMetadata] = useState(null);
   const auth = new AuthService();
   useEffect(() => {
     const getUserMetadata = async () => {
       try {
       const user = await auth.getUserDetails();
-      console.log( user)
+      // console.log( user)
+      setLoading(false);
       setUserMetadata(user);
       } catch (e) {
+        setLoading(false);
         console.log(e);
       }
     };
@@ -24,6 +26,8 @@ const Profile = () => {
 
   // console.log(user);
   return (
+    loading ? <div class="text-center w-100 h50 d-flex align-items-center justify-content-center"><CSpinner /> </div>:
+      
     userMetadata && (
       <div id="profile" className="d-flex flex-column justify-content-center align-items-center">
         <img src={userMetadata?.picture} alt={userMetadata.name}  class="rounded-circle mb-4"/>
